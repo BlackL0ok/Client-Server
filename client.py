5e46f7c61d142a70801e7e47f4539d3427d7b2d2
BlackL0ok <mathys.haubert80@gmail.com>
2022-11-29 21:14:49 +0100

    Z

--- client.py
+++ client.py
@@ -1,16 +1,23 @@
-
-# coding: utf-8
-
 import socket
+def client_program():
+    host = "193.32.126.225"  # as both code is running on same pc
+    port = 55032  # socket server port number
 
-hote = "localhost"
-port = 15555
+    client_socket = socket.socket()  # instantiate
+    client_socket.connect((host, port))  # connect to the server
 
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket.connect((hote, port))
-print ("Connection on {}".format(port))
+    message = input(" -> ")  # take input
 
-socket.send("Hey my name is Olivier!")
+    while message.lower().strip() != 'bye':
+        client_socket.send(message.encode())  # send message
+        data = client_socket.recv(1024).decode()  # receive response
 
-print ("Close")
-socket.close()
+        print('Received from server: ' + data)  # show in terminal
+
+        message = input(" -> ")  # again take input
+
+    client_socket.close()  # close the connection
+
+
+if __name__ == '__main__':
+    client_program()
