16945bbbfcc4856040981f97b8e4a8a0eb85b141
BlackL0ok <mathys.haubert80@gmail.com>
2022-11-29 21:12:39 +0100

    Z

--- client.py
+++ client.py
@@ -8,9 +8,9 @@ port = 15555
 
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.connect((hote, port))
-print "Connection on {}".format(port)
+print ("Connection on {}".format(port))
 
 socket.send("Hey my name is Olivier!")
 
-print "Close"
+print ("Close")
 socket.close()
