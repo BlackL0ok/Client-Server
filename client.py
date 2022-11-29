c9036ce6fd1b0d6de5d6a020cd6a5d393c9bf461
BlackL0ok <mathys.haubert80@gmail.com>
2022-11-29 18:53:02 +0100

    change2

--- client.py
+++ client.py
@@ -1,25 +1,15 @@
+# coding: utf-8
+
 import socket
 
+hote = "192.168.122.1"
+port = 15555
 
-def client_program():
-    host = "192.32.126.255"  # as both code is running on same pc
-    port = 55032  # socket server port number
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket.connect((hote, port))
+print "Connection on {}".format(port)
 
-    client_socket = socket.socket()  # instantiate
-    client_socket.connect((host, port))  # connect to the server
+socket.send("Hey my name is Olivier!")
 
-    message = input(" -> ")  # take input
-
-    while message.lower().strip() != 'bye':
-        client_socket.send(message.encode())  # send message
-        data = client_socket.recv(1024).decode()  # receive response
-
-        print('Received from server: ' + data)  # show in terminal
-
-        message = input(" -> ")  # again take input
-
-    client_socket.close()  # close the connection
-
-
-if __name__ == '__main__':
-    client_program()
+print "Close"
+socket.close()
--- server.py
+++ server.py
@@ -1,29 +1,19 @@
-# Python 3 server example
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import time
+# coding: utf-8
 
-hostName = "localhost"
-serverPort = 8080
+import socket
 
-class MyServer(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
-        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-        self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket.bind(('', 15555))
 
-if __name__ == "__main__":        
-    webServer = HTTPServer((hostName, serverPort), MyServer)
-    print("Server started http://%s:%s" % (hostName, serverPort))
+while True:
+        socket.listen(5)
+        client, address = socket.accept()
+        print "{} connected".format( address )
 
-    try:
-        webServer.serve_forever()
-    except KeyboardInterrupt:
-        pass
+        response = client.recv(255)
+        if response != "":
+                print response
 
-    webServer.server_close()
-    print("Server stopped.")
+print "Close"
+client.close()
+stock.close()
